875a634379e49ced29b05c4fc2103104462b1e78
Entoni Mzilangve <entoni.mzilangve@dualbootpartners.com>
2024-02-19 12:55:35 +0600

    Created Tag & Task ViewSet

--- main/views.py
+++ main/views.py
@@ -1,8 +1,22 @@
 from rest_framework import viewsets
-from models.user import User
-from .serializers import UserSerializer
+from .models import Tag, Task, User
+from .serializers import TagSerializer, TaskSerializer, UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.order_by("id")
     serializer_class = UserSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.order_by("id")
+    serializer_class = TagSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = (
+        Task.objects.select_related("author", "executor")
+        .prefetch_related("tags")
+        .order_by("id")
+    )
+    serializer_class = TaskSerializer
