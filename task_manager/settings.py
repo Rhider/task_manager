f1e1a771ee9a021efd6bb308261856426af846c7
Entoni Mzilangve <entoni.mzilangve@dualbootpartners.com>
2024-08-29 02:15:33 +0300

    Added celery config

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -141,3 +141,6 @@ else:
 UPLOAD_MAX_SIZES: dict[str, int] = {
     "avatar_picture": 1 * 1024 * 1024,
 }
+
+CELERY_BROKER_url = f"redis://{os.environ['REDIS_HOST']}:{os.environ['REDIS_PORT']}/0"
+CELERY_INCLUDE = ["task_manager.tasks.py"]
